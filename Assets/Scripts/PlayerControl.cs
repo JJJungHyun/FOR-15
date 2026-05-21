bc4bed76120df5db3a3840df25072e0be669f653
JJJungHyun <tnwlqqn12345@gmail.com>
2026-05-21 12:40:23 +0900

    0521 - 캐릭터 스프라이트 추가

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -15,37 +15,47 @@ public class PlayerControl : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     void Update()
     {
         // 방향키 입력
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
 
-        // 입력 방향 정규화
-        Vector3 normalizedInput = new Vector3(inputX, inputY, 0).normalized;
+        Vector3 moveDir = Vector3.zero;
+
+        // 한 번에 하나의 축만 입력받도록 처리 (수평 이동에 우선순위)
+        if (inputX != 0)
+        {
+            moveDir = new Vector3(inputX, 0, 0).normalized;
+        }
+        else if (inputY != 0)
+        {
+            moveDir = new Vector3(0, inputY, 0).normalized;
+        }
 
         // 이동 로직
-        transform.Translate(normalizedInput * moveSpeed * Time.deltaTime);
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
 
-        if (inputX == 0 && inputY == 0)
+        // 애니메이션 및 스프라이트 처리
+        if (moveDir == Vector3.zero)
         {
             playerAnimation.SetAnimState(PlayerAnimState.Idle);
         }
         else
         {
-            if (inputY > 0)
+            if (moveDir.y > 0)
             {
                 playerAnimation.SetAnimState(PlayerAnimState.MoveUp);
             }
-            else if (inputY < 0)
+            else if (moveDir.y < 0)
             {
                 playerAnimation.SetAnimState(PlayerAnimState.MoveDown);
             }
-        
-            else if (inputX != 0)
+            else if (moveDir.x != 0)
             {
-                // 오른쪽(>0)이면 flipX를 true, 왼쪽(<0)이면 false
-                spriteRenderer.flipX = (inputX > 0);
+                // 오른쪽이면 flipX를 true, 왼쪽이면 false
+                spriteRenderer.flipX = (moveDir.x > 0);
                 playerAnimation.SetAnimState(PlayerAnimState.MoveLeft);
             }
         }
